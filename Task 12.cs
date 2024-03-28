48382761d57ffbbf83800beaadec9344d7d8f9e4
Albert Avanesyan <117994805+abavanesyan@users.noreply.github.com>
2024-03-28 15:03:55 +0400

    Add files via upload

---
+++ Task 12.cs
@@ -0,0 +1,47 @@
+﻿namespace C__classes
+{
+    abstract class GeneralSort
+    {
+        public void Swap(int i, int j, ref int[] arr)
+        {
+            if (arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    class InsertionS : GeneralSort
+    {
+        public int[] arr = { 4, 3, 5, 1, 7, 2 };
+        public void ins_sort()
+        {
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                for (int j = 1 + i; j < arr.Length; j++)
+                {
+                    Swap(j - 1, j, ref arr);
+                }
+            }
+            Console.WriteLine(arr);
+        }
+
+    }
+
+    class SelectionS : GeneralSort
+    {
+
+    }
+
+    class BubbleS : GeneralSort
+    {
+
+    }
+
+    internal class Task_12
+    {
+        public int[] arr = { 1, 3, 5 };
+    }
+}
